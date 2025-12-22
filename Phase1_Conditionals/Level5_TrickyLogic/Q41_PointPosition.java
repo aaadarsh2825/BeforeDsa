e6f04fe541b52e4a57ff91d79201b04e8620a7e8
Adarsh Tiwari <aaadarsh2825@gmail.com>
2025-12-22 10:57:37 +0530

    Phase 1 Levl 5 question 1

--- Phase1_Conditionals/Level5_TrickyLogic/Q41_PointPosition.java
+++ Phase1_Conditionals/Level5_TrickyLogic/Q41_PointPosition.java
@@ -1,5 +1,26 @@
 package Phase1_Conditionals.Level5_TrickyLogic;
 
+import java.util.Scanner;
+
 public class Q41_PointPosition {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(pointPOsition(x, y));
+        }
+    }
+
+    public static String pointPOsition(int x, int y){
+        if (x== 0 && y==0) {
+            return "origin";
+        }else if (x == 0) {
+            return "y-axis";
+        }else if (y == 0) {
+            return "x-axis";
+        }else{
+            return "others";
+        }
+    }
 }
